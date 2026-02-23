19815dc7a7722afa9e2b1c637ba1a147ce4eede5
Nagoor Nasurudeen <nagoornasurudeen@gmail.com>
2026-02-23 14:06:55 +0530

    included optimum approch

--- L2/Strings/LengthOfLastWord.java
+++ L2/Strings/LengthOfLastWord.java
@@ -3,17 +3,26 @@ public class LengthOfLastWord {
     
     
     public static void main(String[] args) {
-        String s= "Hello world   ";
+        String s= "hello world   ";
         int wordLength=0;
-        int lastSeen=0;
-        for(char c :s.toCharArray()){
-            if (c==' '){
-                if (wordLength!=0 )lastSeen=wordLength;
-                wordLength=0 ;
-            }
-            else wordLength=wordLength+1;
+        // int lastSeen=0;
+        // for(char c :s.toCharArray()){
+        //     if (c==' '){
+        //         if (wordLength!=0 )lastSeen=wordLength;
+        //         wordLength=0 ;
+        //     }
+        //     else wordLength=wordLength+1;
+        // }
+        // System.out.println(wordLength==0?lastSeen:wordLength);
+        int i=s.length()-1;
+        while(i>=0 && s.charAt(i)==' '){
+            i--;
         }
-        System.out.println(wordLength==0?lastSeen:wordLength);
+        while(i>=0 && s.charAt(i)!=' '){
+            wordLength++;
+            i--;
+        }
+        System.out.println(wordLength);
     }
 }
 
